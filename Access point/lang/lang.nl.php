9c627acd84971aecfc61e4b8bb00443dcdd66861
Transport & Mobility Leuven - Telraam <48987469+Telraam@users.noreply.github.com>
2020-07-07 11:26:28 +0200

    v10 image

---
+++ Access point/lang/lang.nl.php
@@ -0,0 +1,25 @@
+<?php
+	$language['welcome_title'] = "Welkom bij je Telraam!";
+	$language["welcome_txt"] = "Noteer dit <b>unieke serienummer</b> van je Telraam. Je hebt het later nodig om de registratie te vervolledigen en je verkeersdata te kunnen bekijken.";
+	$language["livestream_title"] = "Livestream van de sensor";
+	$language["livestream_txt"] = "Richt de camera zodat de weg <b>centraal</b> in beeld is:";
+	$language['serial_number'] = "Serienummer";
+	$language['your_serial_number'] = "Uw serienummer";
+	$language["connect_title"] = "Verbind je Telraam met het internet";
+	$language["connect_txt"] = "Vul de velden hieronder in, let op voor schrijffouten. Hierdoor kan je je Telraam met het internet verbinden via het <b>draadloze thuisnetwerk</b>.";
+	$language["wifi_network"] = "Wifi–netwerk";
+	$language["wifi_pwd"] = "Wifi–paswoord";
+	$language["save"] = "Bewaar";
+	$language["wifi_db_error"] = "Er was een probleem bij het maken van de verbinding met de wifi databank op de Raspberry Pi:";
+	$language["current_wifi_setup"] = "Huidige wifi setup";
+	$language["wifi_data"] = "Hieronder vind je de opgeslagen wifi gegevens";
+	$language["wifi_ssid"] = "SSID (naam van je wifi netwerk):";
+	$language["password"] = "Paswoord:";
+	$language["connection_timer"] = "Je telraam zal over <b>{minutes}</b> minuten en <b>{seconds}</b> seconden een verbinding maken met het ingestelde wifi netwerk...";
+	$language["add_custom_network"] = "Voeg aangepast wifi netwerk toe";
+	$language["custom_network"] = "Eigen, aangepaste netwerk naam";
+	$language["send_background"]= "Telraam mag dagelijks een beeld van mijn straat bijhouden, zodat ik indien nodig gemakkelijk de camera-stand kan controleren";
+	$language["more_info"]="Meer uitleg";
+	$language["send_background_more_info"]="<p>Als je dit vakje aanvinkt, dan kan je in je eigen dashboard de meest recent achtergrondfoto van jouw Telraam raadplegen en kan je eenvoudig  controleren of de camera nog steeds goed gericht is, zonder opnieuw te moeten verbinden met je Telraam. Telraam neemt éénmaal per dag zo'n achtergrondfoto en houdt deze foto 1 maand bij in de database.</p><p>Om privacy van passanten te garanderen passen we 2 technieken toe:<ol><li>We nemen een achtergrond foto gedurende een periode van 30 seconden en nemen de mediaan-waarde per pixel; hierdoor zijn alle bewegende objecten niet zichtbaar</li><li>We vervormen de foto naar (extreem) lage resolutie met behulp van pixelation (https://en.wikipedia.org/wiki/Pixelation)</li></ol></p><p>Als je dit niet wenst, dan kan je nog steeds het camera-beeld van je Telraam controleren, door enkel nog manueel met je Telraam toestel te verbinden via het het Telraam wifi-netwerk: https://telraam.zendesk.com/hc/nl/articles/360026468392-Camera-position-checking-images</p>";
+	$language["save_success"]="Uw instellingen zijn succesvol opgeslagen!";
+?>
